ad17e86b907031bf0dc81a7f0b52c9df91a31564
murash4 <singleant@gmail.com>
2021-01-07 01:21:39 +0300

    add basic layout and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useState } from 'react'
+
 function App() {
+  const [toggle, setToggle] = useState(true)
+
   return (
     <div className="container">
+      <button onClick={ () => setToggle(!toggle) }>Toggle</button>
+      <hr />
+      <div className="blocks">
+        { toggle && <div className="square blue">{ toggle.toString() }</div> }
+      </div>
     </div>
   );
 }
